a2a660ae24656f0fa1b002725325144b6ec58010
Avindra Fernando <avindra1@gmail.com>
2020-06-14 19:49:52 -0500

    Added RTL and First Render Test

--- src/component/App.test.js
+++ src/component/App.test.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+test("renders", () => {
+  render(<App />);
+  expect(screen.getByLabelText("Total")).toHaveTextContent("0");
 });
